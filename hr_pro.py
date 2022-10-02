f2652bcfb096794a5cc7ee0fb96e5a9a50aff946
phalkandari <mohammad.kandary@gmail.com>
2022-10-02 19:16:25 +0300

    Saving for the second time

--- hr_pro.py
+++ hr_pro.py
@@ -1,5 +1,5 @@
 class Employee:
-    """A simple employee class"""
+    """A simple employee class."""
     def __init__(self, name, age, salary, employment_years):
        self.name = name
        self.age = age
@@ -7,16 +7,17 @@ class Employee:
        self.employment_years = employment_years
        
     def get_annual_salary(self, salary):
-        """describes the annual salary/payment of an employee"""
+        """describes the annual salary of an employee."""
         return self.salary * 12
     
     def __str__(self):
+        """returns the string representation of the object."""
         return f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}, Employment Years: {self.employment_years}"
 
 class Manager(Employee):
     def __init__(self, name, age, salary, employment_years, bonus_percentage):
          super().__init__(name, age, salary, employment_years)
-         self.bonus_percentage = self.bonus_percentage * self.salary
+         self.bonus_percentage = bonus_percentage * self.salary
     
     def __str__(self):
         return f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}, Employment Years: {self.employment_years}, Bonus Percentage: {self.bonus_percentage}"
@@ -26,11 +27,54 @@ employee_one = Employee("Laila", 24, 9999, 4)
 employee_two = Employee("Moh", 27, 999, 2)
 
 #managers
-manager_one = Manager("sammy", 52, 4600, 19, 1380.0)
+manager_one = Manager("sammy", 52, 4600, 19, 1380)
+
+def get_options():
+    return ["Show Employees", "Show Managers", "Add An Employee", "Add A Manager", "Exit"]
 
 
-employee_list = []
-manager_list = []
+def show_options(options):
+    print()
+    print("Options:")
+
+    for idx, option in enumerate(options, start=1):
+        print(f"{idx}. {option}")
+
+    print()
+
+
+def HR_Pro():
+    print("Welcome to HR Pro")
+    print("Options: ")
+
+
+
+
+
+# def showing_employees():
+    
+# def showing_managers():
+    
+def adding_an_employee(employees):
+    employee_list = [employee_one, employee_two]
+    for employee in employees:
+        employee_list.append(employee)
+    
+    
+def adding_a_manager(managers):
+    manager_list = [manager_one]
+    for manager in managers:
+        manager_list.append(manager)
+
+
+# def Exiting():
+    
+    
+    
+    
+    
+    
+
 
 
 
@@ -39,7 +83,11 @@ manager_list = []
 
 
 def main():
-	# main code here
+    print(
+        "Welcome to HR Pro"
+        "Options:"
+    )
+    options = get_options()
 
-if __name__ == '__main__':
-	main()
+# if __name__ == '__main__':
+# 	main()

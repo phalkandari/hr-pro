1b4fa47c91efde12b60a2750465a95bf24730991
phalkandari <mohammad.kandary@gmail.com>
2022-10-06 17:41:54 +0300

    Solution without the while loop

--- hr_pro discussion on Thursday.py
+++ hr_pro discussion on Thursday.py
@@ -10,127 +10,67 @@ class Employee:
         self.salary = salary
         self.employment_years = employment_years
 
-    def get_annual_salary(self, salary):
-        """describes the annual salary of an employee."""
-        self.salary = salary
-        return self.salary * 12
-
     def __str__(self):
         """returns the string representation of the object."""
         return f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}, Employment Years: {self.employment_years}"
+
+    def get_annual_salary(self):
+        """describes the annual salary of an employee."""
+        return self.salary * 12
 
 
 class Manager(Employee):
     def __init__(self, name, age, salary, employment_years, bonus_percentage):
         super().__init__(name, age, salary, employment_years)
-        self.bonus_percentage = bonus_percentage * self.salary
+        self.bonus_percentage = bonus_percentage
 
     def __str__(self):
-        return f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}, Employment Years: {self.employment_years}, Bonus Percentage: {self.bonus_percentage}"
+        return super().__str__()+f" Bonus: {self.bonus_percentage}"
 
-
-# employees
-employee_one = Employee("Laila", 24, 9999, 4)
-employee_two = Employee("Moh", 27, 999, 2)
-
-# managers
-manager_one = Manager("sammy", 52, 4600, 19, 1380.0)
-
-
-# To itirate over each option and number it
-def get_options():
-    return ["Show Employees", "Show Managers", "Add An Employee", "Add A Manager", "Exit"]
-
-
-def show_options(options):
-    print()
-    print("Options:")
-
-    for idx, option in enumerate(options, start=1):
-        print(f"{idx}. {option}")
-
-    print()
-
-
-employee_list = [employee_one, employee_two]
-manager_list = [manager_one]
-
-# The Function asking the user to choose an option
-
-
-def asking_for_user_option():
-    option = int(input("What would you like to do? "))
-    if option == 1:
-        show_employees()
-    elif option == 2:
-        show_managers()
-    elif option == 3:
-        add_an_employee(employee_list)
-    elif option == 4:
-        add_a_manager(manager_list)
-    elif option == 5:
-        Exit()
-
-# The Function 1
-
-
-def show_employees():
-    print("Employees")
-    print(" ")
-    for empo in employee_list:
-        print(empo)
-
-# The Function 2
-
-
-def show_managers():
-    print("Managers")
-    print(" ")
-    for mano in manager_list:
-        print(mano)
-
-# The Function 3
-
-
-def add_an_employee(employee_list):
-    name = input("Name: ")
-    age = int(input("Age: "))
-    salary = int(input("Salary: "))
-    employment_years = int(input("Employment years: "))
-    added_employee = Employee(name, age, salary, employment_years)
-    employee_list.append(added_employee)
-    print("Employee added successfully")
-    print(added_employee)
-    return employee_list
-
-# The Function 4
-
-
-def add_a_manager(manager_list):
-    name = input("Name: ")
-    age = int(input("Age: "))
-    salary = int(input("Salary: "))
-    employment_years = int(input("Employment years: "))
-    bonus_percentage = float(input("Bonus Percentage: "))
-    added_manager = Manager(
-        name, age, salary, employment_years, bonus_percentage)
-    manager_list.append(added_manager)
-    print("Manager added successfully")
-    print(added_manager)
-    return manager_list
-
-# The Function 5
-
-
-def Exit():
-    quit()
+    def get_bonus(self):
+        return self.bonus_percentage * self.salary
 
 
 def main():
-    print("Welcome to HR Pro")
-    print("Options:")
-    show_options(get_options())
-    asking_for_user_option()
+    employees_list = []
+    managers_list = []
+    print('''Options:
+          1. Show Employees
+          2. Show Managers
+          3. Add An Employee
+          4. Add A Manager
+          5. Exit''')
+
+    choice = int(input("What would you like to do? "))
+
+    if choice == 1:
+        for employee in employees_list:
+            print(employee)
+    elif choice == 2:
+        for manager in managers_list:
+            print(manager)
+    elif choice == 3:
+        name = input("Name: ")
+        age = int(input("Age: "))
+        salary = int(input("Salary: "))
+        employment_years = int(input("Employment years: "))
+        added_employee = Employee(name, age, salary, employment_years)
+        employees_list.append(added_employee)
+        print("Employee added successfully")
+        print(added_employee)
+    elif choice == 4:
+        name = input("Name: ")
+        age = int(input("Age: "))
+        salary = int(input("Salary: "))
+        employment_years = int(input("Employment years: "))
+        bonus_percentage = float(input("Bonus Percentage: "))
+        added_manager = Manager(
+            name, age, salary, employment_years, bonus_percentage)
+        managers_list.append(added_manager)
+        print("Manager added successfully")
+        print(added_manager)
+    else:
+        print("Invalid input!")
 
 
 if __name__ == '__main__':
